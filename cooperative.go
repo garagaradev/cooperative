97110c89ad48d4f82d37083d457d05d11de87c58
garagaradev <garagaradev@proton.me>
2024-10-05 00:53:21 +0700

    Added SaveMoney()

--- cooperative.go
+++ cooperative.go
@@ -29,3 +29,14 @@ func (c *Cooperative) AddMember(name string){
   fmt.Printf("New member added with ID %d and name %s.\n", newMember.ID, newMember.Name)
 }
 
+func (c *Cooperative) SaveMoney(id int, amount float64){
+  for i, member := range c.MemberList {
+    if member.ID == id {
+      c.MemberList[i].Savings += amount
+      fmt.Printf("Rp%.2f saved to %s' account. Total savings:Rp%.2f\n", amount, member.Name, c.MemberList[i].Savings)
+      return
+    }
+  }
+  fmt.Println("Member Not Found.")
+}
+
